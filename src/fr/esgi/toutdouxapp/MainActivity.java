55355067f853d02e5d9ddeb99233cd3060041255
Rémy Hannequin <remy.hannequin@gmail.com>
2014-01-16 20:51:40 +0100

    Clean

--- src/fr/esgi/toutdouxapp/MainActivity.java
+++ src/fr/esgi/toutdouxapp/MainActivity.java
@@ -24,11 +24,11 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+
         requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
         setContentView(R.layout.activity_main);
         getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.window_title);
-        
+
         listView = (ListView) findViewById(R.id.list);
         List<Task> tasks = setListTasks();
 
@@ -44,12 +44,6 @@ public class MainActivity extends Activity {
             }
         });
     }
-
-//    public void onClickTaskItem(View v) {
-//        Intent taskActivityIntent = new Intent(this, TaskActivity.class);
-//        taskActivityIntent.putExtra("task", setListTasks().get(0));
-//        startActivity(taskActivityIntent);
-//    }
 
     private List<Task> setListTasks() {
         List<Task> tasks = new ArrayList<Task>();
--- src/fr/esgi/toutdouxapp/TaskActivity.java
+++ src/fr/esgi/toutdouxapp/TaskActivity.java
@@ -16,7 +16,7 @@ public class TaskActivity extends Activity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    
+
     requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
     setContentView(R.layout.activity_task);
     getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.window_title);
